0c263508f589b00aec9545584afadda5b5a6479a
hye0e <nanda5050@naver.com>
2022-07-02 01:03:27 +0900

    [백엔드]네이버 api 추가

--- bookHub-backend/controller/books.js
+++ bookHub-backend/controller/books.js
@@ -1,11 +1,40 @@
 import * as bookRepository from "../data/books.js";
 
+const request = require("request");
+
+const client_id = "E_7i0KfAYDXbZGXED7wh";
+const client_secret = "I73xFgMJIc";
+
 export async function getBooks(req, res) {
   const bookname = req.query.bookname;
-  const data = await (bookname
-    ? bookRepository.getAllByBookname(bookname)
-    : bookRepository.getAll());
-  res.status(200).json(data);
+  const option = {
+    d_titl: bookname
+  };
+  const options = {
+    uri: "https://openapi.naver.com/v1/search/book_adv.json", 
+    qs: option,
+    headers: {
+      'X-Naver-Client-Id': client_id,
+      'X-Naver-Client-Secret': client_secret
+    }
+  }
+
+  request.get(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      let json = JSON.parse(body) 
+
+      res.status(200).json(json);
+    } else {
+      res.status(response.statusCode).end();
+      console.log('error = ' + JSON.stringify(response));
+    }
+  })
+
+  // const data = await (bookname
+  //   ? bookRepository.getAllByBookname(bookname)
+  //   : bookRepository.getAll());
+
+  // res.status(200).json(data);
 }
 
 export async function getById(req, res) {
