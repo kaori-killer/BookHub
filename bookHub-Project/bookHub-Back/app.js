d046a765ad4abe977a0ac633033328aac083937c
hye0e <nanda5050@naver.com>
2022-07-06 11:36:24 +0900

    [백엔드] img url CSP

--- bookHub-Project/bookHub-Back/app.js
+++ bookHub-Project/bookHub-Back/app.js
@@ -9,6 +9,7 @@ import bookRouter from "./router/books.js";
 import authRouter from "./router/auth.js";
 import searchRouter from "./router/search.js";
 import wishRouter from "./router/wish.js";
+import statisticsRouter from "./router/statistics.js";
 import viewerRouter from "./router/viewer.js";
 import testRouter from "./router/test.js";
 
@@ -25,11 +26,24 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(
+	helmet.contentSecurityPolicy({
+		useDefaults: true,
+		directives: {
+			'img-src': ["'self' data:", '*.bookthumb-phinf.pstatic.net']
+		}
+	})
+);
+app.use((req, res, next) => {
+	res.removeHeader("Cross-Origin-Embedder-Policy");
+	next();
+});
 
 app.use("/books", bookRouter);
 app.use("/auth", authRouter);
 app.use("/search", searchRouter);
 app.use("/wishs", wishRouter);
+app.use("/statistics", statisticsRouter);
 app.use("/test", testRouter);
 app.use("/", viewerRouter);
 
